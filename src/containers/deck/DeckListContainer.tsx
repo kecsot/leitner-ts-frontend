e1c4dab665be1a6faf283afe06f8a5cfa9fa4f2f
kecsot <kecsot08@gmail.com>
2023-08-15 20:39:11 +0200

    refactor Loading progress on DeckListContainer

--- src/containers/deck/DeckListContainer.tsx
+++ src/containers/deck/DeckListContainer.tsx
@@ -1,28 +1,25 @@
-import { useNavigate, useNavigation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { DeckList } from "../../components/deck/DeckList";
 import { useDeckListQuery } from "../../queries/deck";
 import { DeckType } from "../../api/types";
-import { routes } from "../../router";
-
+import { LoadingProgressBar } from "../../components/base/LoadingProgressBar";
 
 export const DeckListContainer = () => {
     const navigate = useNavigate();
-    const {isLoading, error, data} = useDeckListQuery()
-
-    // TODO: errorBoundary
-
-
+    const { isLoading, error, data } = useDeckListQuery()
 
     const onItemClicked = (deck: DeckType) => {
-       navigate('/decks/detail/' + deck.id)
+        navigate('/decks/detail/' + deck.id)
     }
 
     return (
         <>
-            {isLoading && <p>Loading...</p>}
-            {data?.data && 
-                <DeckList 
-                    items={data.data} 
+            <LoadingProgressBar 
+                isLoading={isLoading}/>
+
+            {data?.data &&
+                <DeckList
+                    items={data.data}
                     onViewItem={onItemClicked} />
             }
         </>
