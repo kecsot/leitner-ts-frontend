0708a734d7510af3db41de4d6106f73d185d075c
kecsot <kecsot08@gmail.com>
2023-08-16 21:08:44 +0200

    wrap Router with ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import { QueryClient, QueryClientProvider } from "react-query"
-import { Router } from "./router/router"
+import {QueryClient, QueryClientProvider} from "react-query"
+import {Router} from "./router/router"
+import {ErrorBoundary} from "react-error-boundary";
 
 function App() {
-  const queryClient = new QueryClient()
+    const queryClient = new QueryClient()
 
-  queryClient.setDefaultOptions({
-    queries: {
-      staleTime: 1000 * 5,
-      cacheTime: 1000 * 8,
-    },
-  })
+    queryClient.setDefaultOptions({
+        queries: {
+            staleTime: 1000 * 5,
+            cacheTime: 1000 * 8,
+        },
+    })
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Router />
-    </QueryClientProvider>
-  )
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ErrorBoundary fallback={<div>Something went wrong</div>}>
+                <Router/>
+            </ErrorBoundary>
+        </QueryClientProvider>
+    )
 }
 
 export default App
