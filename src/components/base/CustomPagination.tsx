f4840bcdd05d9fe0a3d6bcaefdfc66b2b7ae9cc4
kecsot <kecsot08@gmail.com>
2023-08-23 22:56:01 +0200

    fix summary typo

--- src/components/base/CustomPagination.tsx
+++ src/components/base/CustomPagination.tsx
@@ -8,7 +8,7 @@ export type CustomPaginationProps = UsePaginationProps & {
 
 /**
  * This custom component calculate the number of pages based on the total number of items and the number of items per page.
- * Now it works similar then TablePagination component.
+ * Now it works similar than TablePagination component.
  *
  * @param total
  * @param itemsPerPage
