5b7862dadb8ea8081e7c2d9644b75f23a183bbd3
kecsot <kecsot08@gmail.com>
2023-08-23 22:53:31 +0200

    create CustomPagination with a helper hook.

---
+++ src/hook/useCustomPaginationProps.ts
@@ -0,0 +1,32 @@
+import React, {useMemo, useState} from "react";
+import {PaginationRequestParams} from "../@types/base.ts";
+import {CustomPaginationProps} from "../components/base/CustomPagination.tsx";
+
+type Props = {
+    defaultPage?: number,
+    itemsPerPage: number,
+}
+
+export const useCustomPaginationProps = ({defaultPage = 1, itemsPerPage}: Props) => {
+    const [page, setPage] = useState<number>(defaultPage)
+
+    const tablePaginationProps = useMemo(() => {
+        return {
+            page,
+            itemsPerPage,
+            onChange: (_event: React.ChangeEvent<unknown> | null, page: number) => setPage(page),
+        } as Omit<CustomPaginationProps, 'total'>
+    }, [page, itemsPerPage])
+
+    const paginationRequestParams: PaginationRequestParams = useMemo(() => {
+        return {
+            page,
+            limit: itemsPerPage
+        }
+    }, [page, itemsPerPage])
+
+    return {
+        tablePaginationProps,
+        paginationRequestParams
+    }
+}
