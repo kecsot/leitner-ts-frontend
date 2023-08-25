33a4e2ba9d876aa8e1e4269eb93abc2ad0e37c9a
kecsot <kecsot08@gmail.com>
2023-08-25 22:02:45 +0200

    add some random image to AchievementItem

--- src/components/achievement/AchievementItem.tsx
+++ src/components/achievement/AchievementItem.tsx
@@ -1,17 +1,46 @@
+import {Avatar} from "@mui/material";
 import {AchievementType} from "../../@types/achievement.ts";
+import {LazyLoadImage} from "react-lazy-load-image-component";
 
 type AchievementItemProps = {
     achievement: AchievementType
 }
 
 export const AchievementItem = ({achievement}: AchievementItemProps) => {
+
+    const getImageByAchievementKey = (key: string) => {
+        // TODO: get thumbnail by achievement key
+        return `https://picsum.photos/100/100?random=${key}` + Math.random()
+    }
+
+    const getThumbnailByAchievementKey = (key: string) => {
+        // TODO: get thumbnail by achievement key
+        return `https://picsum.photos/20/20?random=${key}` + Math.random()
+    }
+
     return (
         <div>
-            {achievement.name}
-            {achievement.description}
-
-            {achievement.acquired.toString()}
-            {achievement.acquiredAt?.toISOString()}
+            <Avatar
+                sx={{
+                    width: 100,
+                    height: 100,
+                    filter: achievement.acquired ? 'none' : 'grayscale(100%)',
+                    opacity: achievement.acquired ? 1 : 0.2,
+                    border: achievement.acquired ? '2px solid yellow' : '5px solid red'
+                }}
+            >
+                <LazyLoadImage src={getImageByAchievementKey(achievement.key)}
+                               placeholderSrc={getThumbnailByAchievementKey(achievement.key)}
+                               width={100}
+                               height={100}
+                               effect='blur'
+                               alt="Image Alt"
+                />
+            </Avatar>
+            <div>{achievement.name}</div>
+            <div>{achievement.description}</div>
+            <div>{achievement.acquired.toString()}</div>
+            <div>{achievement.acquiredAt?.toISOString()}</div>
         </div>
     )
 }
