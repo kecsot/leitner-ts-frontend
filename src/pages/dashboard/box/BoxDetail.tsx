9c475c0d614c73184fa98dff1428caec47253dc5
kecsot <kecsot08@gmail.com>
2023-09-15 20:31:44 +0200

    allow edit, delete from BoxDetail page

--- src/pages/dashboard/box/BoxDetail.tsx
+++ src/pages/dashboard/box/BoxDetail.tsx
@@ -1,12 +1,28 @@
-import {useParams} from "react-router-dom"
+import {useNavigate, useParams} from "react-router-dom"
 import {BoxDetailContainer} from "../../../containers/box/BoxDetailContainer.tsx"
 import {BoxCardListContainer} from "../../../containers/card/BoxCardListContainer.tsx";
 import Page404 from "../../Page404.tsx";
-import {Container} from "@mui/material";
+import {Button, Container, Dialog, DialogContent, DialogTitle} from "@mui/material";
 import {Helmet} from "react-helmet";
+import {BoxFormEditContainer} from "../../../containers/box/BoxFormEditContainer.tsx";
+import {BoxDeleteFormContainer} from "../../../containers/box/BoxDeleteFormContainer.tsx";
+import {useCrudOperation} from "../../../hook/useCrudOperation.ts";
 
 const BoxDetail = () => {
     const {id} = useParams();
+    const navigate = useNavigate()
+
+    const {
+        selectEditOperation,
+        selectDeleteOperation,
+        resetOperationSelection,
+
+        isEditOperation,
+        isDeleteOperation,
+        isOperationSelected
+    } = useCrudOperation<number>()
+
+    const handleOnDeleteSuccess = () => navigate('/boxes')
 
     if (!id) return <Page404/>;
 
@@ -16,8 +32,43 @@ const BoxDetail = () => {
                 <title>Box detail</title>
             </Helmet>
 
+            <Button
+                variant='outlined'
+                onClick={() => selectEditOperation()}>
+                Edit Box
+            </Button>
+
+            <Button
+                variant='outlined'
+                onClick={() => selectDeleteOperation()}>
+                Delete Box
+            </Button>
+
             <BoxDetailContainer boxId={Number(id)}/>
             <BoxCardListContainer boxId={Number(id)}/>
+
+            <Dialog
+                open={isOperationSelected}
+                fullWidth>
+                <DialogTitle>
+                    {isEditOperation && 'Edit box'}
+                    {isDeleteOperation && 'Delete box'}
+                </DialogTitle>
+                <DialogContent>
+                    {isEditOperation && (
+                        <BoxFormEditContainer
+                            boxId={Number(id)}
+                            onSuccess={resetOperationSelection}
+                            onCancel={resetOperationSelection}/>
+                    )}
+                    {isDeleteOperation && (
+                        <BoxDeleteFormContainer
+                            id={Number(id)}
+                            onSuccess={handleOnDeleteSuccess}
+                            onCancel={resetOperationSelection}/>
+                    )}
+                </DialogContent>
+            </Dialog>
         </Container>
     )
 }
